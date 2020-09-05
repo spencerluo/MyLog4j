b79da5f68b081ef8e8ddb40a19cb50a4eabebc08
spencer <wcnmbwsw@hotmail.com>
2020-09-05 15:55:46 +0800

    update

---
+++ core/src/main/java/api/LoggerContext.java
@@ -0,0 +1,6 @@
+package api;
+
+public interface LoggerContext {
+
+    Logger getLogger(String name);
+}
--- core/src/test/java/common/TestSimpleLogger.java
+++ core/src/test/java/common/TestSimpleLogger.java
@@ -1,15 +1,18 @@
 package common;
 
+import api.Logger;
+import api.LoggerContext;
+import context.SimpleContext;
 import logger.SimpleLogger;
 
 public class TestSimpleLogger {
 
     public static void main(String[] args) {
-        SimpleLogger simpleLogger = new SimpleLogger("test");
+        LoggerContext context = new SimpleContext();
+        Logger simpleLogger = context.getLogger("spencer");
         simpleLogger.info("info");
         simpleLogger.debug("debug");
         simpleLogger.info(new People("spencer", 4));
 
-        simpleLogger.getEntries().forEach(System.out::println);
     }
 }
